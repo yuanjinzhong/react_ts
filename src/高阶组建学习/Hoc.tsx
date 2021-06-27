02e6f1f7f5ba00b98099bc30a51515c8989fc4af
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2021-06-27 09:55:25 +0800

    高阶组件学习

--- src/高阶组建学习/Hoc.tsx
+++ src/高阶组建学习/Hoc.tsx
@@ -3,27 +3,27 @@
  *
  * 高阶组建定义: 一个函数，入参是一个组建,返回也是一个组建。则该函数称为高价组建
  */
-import * as React from "react";
+import React from "react";
 
 /**
  * 此处定义高阶段组建
  * @param Component
  */
-const withSize = (Component) => {
-    return class withSize extends Component {
+const withSize = (component:React.Component) => {
+    return class toSize extends React.Component {
         render() {
-            return <Component/>
+            return <Component/>    //todo   https://segmentfault.com/a/1190000019153177 学习 高阶组件
         }
     }
 }
 
-class Sub extends React.Component<any, any> {
+class Sub extends React.Component {
     render() {
         return <p>我是Sub组建</p>;
     }
 }
 
-class Pub extends React.Component<any, any> {
+class Pub extends React.Component{
     render() {
         return <button>我是Pub组建</button>;
     }
