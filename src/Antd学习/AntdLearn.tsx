60d6f2b7e2f78852a131cb50abb8c465d71fecc1
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2021-07-20 14:44:43 +0800

    antd datepicker学习

--- src/Antd学习/AntdLearn.tsx
+++ src/Antd学习/AntdLearn.tsx
@@ -1,5 +1,12 @@
-import {Button, DatePicker, version} from 'antd';
-import React from 'react';
+import {Button, ConfigProvider, DatePicker, message, version} from 'antd';
+import React, {useState} from 'react';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+
+moment.locale('zh-cn');
 
 const AntdLearn: React.FC<{ msg: string }> = (x: { msg: string }) => {
     return (
@@ -11,4 +18,23 @@ const AntdLearn: React.FC<{ msg: string }> = (x: { msg: string }) => {
     );
 };
 
-export default AntdLearn;
+
+const ShowDate = () => {
+    const [date, setDate] = useState(null);
+    const handleChange = (value) => {
+        message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
+        setDate(value);
+    };
+    return (
+        <ConfigProvider locale={zhCN}>
+            <div style={{width: 400, margin: '100px auto'}}>
+                <DatePicker onChange={handleChange}/>
+                <div style={{marginTop: 16}}>
+                    当前日期：{date != null ? date.format('YYYY年MM月DD日') : '未选择'}
+                </div>
+            </div>
+        </ConfigProvider>
+    );
+};
+
+export {AntdLearn, ShowDate};
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import Father from "./父子组件通信/Father";
 import {Provider} from "./跨组件通信/Context";
 import Son from "./跨组件通信/Son";
 import Main from "./路由学习/RouterLearn";
-import AntdLearn from "./Antd学习/AntdLearn";
+import {AntdLearn, ShowDate} from "./Antd学习/AntdLearn";
 
 ReactDOM.render(
     /*<Counter/>*/
@@ -19,7 +19,7 @@ ReactDOM.render(
     // </Provider>
     /*<Son msg={'【跨组件通信，通过props一层层传递】'}/>*/
     // <Main msg={"我是msg(虽然里面不使用)"}/>
-    <AntdLearn msg={"点击我"}/>
+    <ShowDate/>
     ,
     document.getElementById('root')
 );
