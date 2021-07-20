103c3a3d152874e47d345a7b4a7528327bd0fa2b
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2021-07-20 15:17:11 +0800

    antd datepicker学习

--- src/Antd学习/AntdLearn.tsx
+++ src/Antd学习/AntdLearn.tsx
@@ -22,6 +22,7 @@ const AntdLearn: React.FC<{ msg: string }> = (x: { msg: string }) => {
 const ShowDate = () => {
     const [date, setDate] = useState(null);
     const handleChange = (value) => {
+        console.log(value);
         message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
         setDate(value);
     };
@@ -30,7 +31,7 @@ const ShowDate = () => {
             <div style={{width: 400, margin: '100px auto'}}>
                 <DatePicker onChange={handleChange}/>
                 <div style={{marginTop: 16}}>
-                    当前日期：{date != null ? date.format('YYYY年MM月DD日') : '未选择'}
+                    当前日期：{date ? date.format('YYYY年MM月DD日') : '未选择'}
                 </div>
             </div>
         </ConfigProvider>
